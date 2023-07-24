25e503a6b8d57ef75124e391028bf47c52f0ff65
Yashraj2525 <76719689+Yashraj2525@users.noreply.github.com>
2023-07-24 20:17:54 +0530

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -16,10 +16,10 @@ wordcloud = WordCloud(mask=mask, contour_width=3, contour_color='black', stopwor
 # Generate word cloud
 wordcloud.generate(text)
 
-# Get array representation
+# Get array representation To view the input after running the code instead of saving it to file
 wc_array = wordcloud.to_array()
 
-# Save word cloud instead of showing the output it save to the file name "custom_wordcloud.png"
+# To Save word cloud into png format uncomment the below line and comment the above line. So, it will save to the file named "custom_wordcloud.png"
 # wordcloud.to_file('custom_wordcloud.png')
 
 # Show word cloud
