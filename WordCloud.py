1489ae243fd61db4954b23c1f678ff4627050b46
Yashraj2525 <76719689+Yashraj2525@users.noreply.github.com>
2023-07-24 20:02:26 +0530

    Add files via upload

---
+++ WordCloud.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import numpy as np
+from PIL import Image
+
+# Read text
+text = open('sampletext1.txt').read()
+
+# Read mask image
+mask = np.array(Image.open('cloud.png').convert('L'))
+
+# Create word cloud
+wordcloud = WordCloud(mask=mask, contour_width=3, contour_color='black', stopwords=set(STOPWORDS),
+                      background_color='white', max_words=200, min_font_size=10)
+
+# Generate word cloud
+wordcloud.generate(text)
+
+# Save word cloud
+wordcloud.to_file('custom_wordcloud.png')
+
+# Show word cloud
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
